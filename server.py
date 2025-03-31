85ae3501e81e7fa8ac308ed617e8ebdfb84ea01c
wacinev <wacinev.804@gmail.com>
2025-03-31 14:54:43 -0400

    setup prompt for the role

--- server.py
+++ server.py
@@ -1,19 +1,54 @@
-from flask import Flask, render_template
-
-# OPEN_API_KEY = 
+from flask import Flask, render_template, request, jsonify
+import openai, os
 
 app = Flask(__name__)
+
+OPEN_API_KEY = os.getenv("API_KEY")
+
+client = openai.Client(api_key=OPEN_API_KEY)
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/upload", methods=["POST"])
-def upload():
+@app.route("/chat-with-ai", methods=["POST"])
+def chat():
+    user_chat_msg = request.json.get("user_chat_msg")
+    conversation = request.json.get("conversation")
+    with open("./static/text/section599.md", "r") as file:
+        guidlines = file.read()
+    if len(conversation) == 0:
+        conversation.append({
+            "role": "system",
+            "content": f"You are a compliance auditor professional in quality assurance for reviewing therapy sessions. Ensuring all guidelines and proper procedure is taken working with therapy and NYC guidelines. Use this guidline to help with your answers to questions about cases, medical assistance, staffing, definitions, etc.:\n\n{guidlines}"
+        })
+    conversation.append({
+        "role": "user",
+        "content": user_chat_msg
+    })
 
+    ai_msg = send_convo_to_ai(conversation)
+    print("ai_msg", ai_msg)
 
-    return 
+    conversation.append({
+        "role": "assistant",
+        "content": ai_msg
+    })
+
+    return jsonify({"ai_msg": ai_msg, 
+                    "conversation": conversation})
     
+def send_convo_to_ai(conversation):
+    try:
+        response = client.chat.completions.create(
+            model = "gpt-4o",
+            messages=conversation
+        )
+        return response.choices[0].message.content
+    
+    except Exception as e:
+        return str(e)
+
 
 if __name__ == "__main__":
     app.run(debug=False)
